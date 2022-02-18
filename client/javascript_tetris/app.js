3e472a831d4bd3e9d80184e9c51b3a3ad7ae2d50
Amy Sylla <54957890+amysylla972@users.noreply.github.com>
2022-02-18 14:19:33 -0500

    js work, array + selectors

--- client/javascript_tetris/app.js
+++ client/javascript_tetris/app.js
@@ -1,3 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable padded-blocks */
+/* eslint-disable-next-line no-trailing-spaces */
 document.addEventListener('DOMContentLoaded', () => {
+  // Select grid class in the html 
+  const grid = document.querySelector('.grid');
+  // eslint-disable-next-line prefer-const
+  // Put the div into an array
+  // eslint-disable-next-line prefer-const
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartButton = document.querySelector('#start-button');
+  const width = 10;
+
+  console.log(squares);
 
 });
