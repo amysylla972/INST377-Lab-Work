0bc722e97efdcdb84a81b4062316b416c9af20ee
Amy Sylla <54957890+amysylla972@users.noreply.github.com>
2022-02-24 06:12:11 -0500

    tetrominos_move

--- client/javascript_tetris/app.js
+++ client/javascript_tetris/app.js
@@ -1,3 +1,12 @@
+/* eslint-disable indent */
+/* eslint-disable semi */
+/* eslint-disable comma-spacing */
+/* eslint-disable arrow-parens */
+/* eslint-disable space-in-parens */
+/* eslint-disable space-before-blocks */
+/* eslint-disable prefer-const */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable no-unused-vars */
 /* eslint-disable padded-blocks */
 /* eslint-disable-next-line no-trailing-spaces */
@@ -10,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let squares = Array.from(document.querySelectorAll('.grid div'));
   const ScoreDisplay = document.querySelector('#score');
   const StartButton = document.querySelector('#start-button');
-  const width = 10;
+  const GRID_WIDTH = 10;
 
   // The Tetrominoes
   const lTetromino = [
@@ -47,9 +56,50 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
     [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
   ];
-  
+  // Tetrominoes shapes stored in array
   const theTetrominoes = [lTetromino,iTetromino,zTetromino,oTetromino];
-
+  // randomly selects shape of tetromino
+  let currentPosition = 4;
+  let currentRotation = 0;
   
+  let random = Math.floor(Math.random()*theTetrominoes.length);
+  let current = theTetrominoes[random][0];
+  // Draw tetromino
+  function draw(){
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+    })
+  }
+  
+  function undraw(){
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
 
-});
+  // Stop the tetromino at the end of the grid and draw a new one
+   function freeze(){
+     if (current.some(index => squares[currentPosition + index + GRID_WIDTH].classList.contains('taken'))){
+       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+       // start new tetromino
+      
+     random = Math.floor(Math.random()*theTetrominoes.length);
+     current = theTetrominoes[random][currentRotation];
+     currentPosition = 4;
+     draw()
+     }
+   }
+
+   // Move down the tetromino
+
+   function moveDown(){
+    undraw()
+    currentPosition += GRID_WIDTH
+    draw()
+    freeze()
+    }
+    // Set falling speed
+    timerId = setInterval(moveDown, 1000)
+
+ });
+
