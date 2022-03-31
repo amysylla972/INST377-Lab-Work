b1bc8fcf994f157e661bdef3938d1e697f01c4e6
Amy Sylla <54957890+amysylla972@users.noreply.github.com>
2022-03-31 04:47:51 -0400

    lab 6 script ok

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,14 +1,51 @@
-async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-multi-assign */
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
+}
+
+function dataHandler(arr) {
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, arr.length - 1);
+    return arr[restNum];
   });
+  return listItems;
+}
+
+function createList(collection) {
+  console.log(collection);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML= '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
+async function mainEvent() { // the async keyword means we can make API requests
+  const form = document.querySelector('.form-block');
+  const submitButton = document.querySelector('button[type="submit"]');
+  submitButton.style.display = 'none';
+
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+
+  if (arrayFromJson.data.length > 0) {
+    submitButton.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      const restoArray = dataHandler(arrayFromJson.data);
+      createList(restoArray);
+    });
+  }
 }
 
 // this actually runs first! It's calling the function above
